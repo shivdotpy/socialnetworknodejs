7256f88a5ec5270786e4bccccf5eec6a496860b6
shiv <shivssharma1990@gmail.com>
2021-08-03 21:11:12 +0530

    basic structure

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const chalk = require("chalk");
+const cors = require("cors");
+
+const { PORT } = require("./src/utils/settings");
+const { WELCOME_MESSAGE, USER_ROUTE } = require("./src/utils/constants");
+
+// Routes
+const UserRoutes = require("./src/routes/user.route");
+
+const app = express();
+
+app.use(cors());
+
+app.use(USER_ROUTE, UserRoutes);
+
+app.listen(PORT, () => {
+  console.log(chalk.white.bgBlack.bold(WELCOME_MESSAGE));
+});
---
+++ src/controllers/user.controller.js
@@ -0,0 +1,6 @@
+exports.signup = (req, res) => {
+  return res.status(200).send({
+    error: false,
+    message: "App working fine",
+  });
+};
