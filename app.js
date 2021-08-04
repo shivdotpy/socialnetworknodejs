8aaf5b0f71dcbb8d158e96a4236d62ceaa869d1b
shiv <shivssharma1990@gmail.com>
2021-08-04 09:06:58 +0530

    CICD

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const chalk = require("chalk");
 const cors = require("cors");
 
 const { PORT } = require("./src/utils/settings");
-const { WELCOME_MESSAGE, USER_ROUTE } = require("./src/utils/constants");
+const {
+  WELCOME_MESSAGE,
+  USER_ROUTE,
+  INITIAL_PAGE_MESSAGE,
+} = require("./src/utils/constants");
 
 // Routes
 const UserRoutes = require("./src/routes/user.route");
@@ -12,9 +16,9 @@ const app = express();
 
 app.use(cors());
 
-app.get("/", () => {
-  console.log("Application is working");
-});
+app.get("/", () =>
+  res.status(200).send({ error: false, message: INITIAL_PAGE_MESSAGE })
+);
 
 app.use(USER_ROUTE, UserRoutes);
 
