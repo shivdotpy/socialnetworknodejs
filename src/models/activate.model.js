d0d4758b77d18927169cf40f7944d1bcd8835d93
shiv <shivssharma1990@gmail.com>
2021-08-05 21:06:47 +0530

    added timestamp

--- src/models/activate.model.js
+++ src/models/activate.model.js
@@ -1,12 +1,15 @@
 const { Schema, model } = require("mongoose");
 
-const activateModel = Schema({
-  email: {
-    type: String,
+const activateModel = Schema(
+  {
+    email: {
+      type: String,
+    },
+    code: {
+      type: String,
+    },
   },
-  code: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = model("activate", activateModel);
